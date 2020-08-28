04f62ec06a433a3c49ea36cba2dd717fa9d564ad
Ayush Tripathi <ayush@becoder.com>
2020-08-28 12:41:32 +0530

    Plz This time

--- SchoolShow/settings.py
+++ SchoolShow/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2ws%2a8=gza+p56&e2zwfgi6bfs%tgiu9trhq)6mnjppzc7#8)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
